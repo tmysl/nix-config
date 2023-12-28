8fac3654ec407aff81b29dffb6c87daa30899e37
Brandon Gottlob <info@bgottlob.com>
2023-12-27 22:37:45 -0500

    Add NixOS configuration file

--- home.nix
+++ home.nix
@@ -6,14 +6,8 @@
   home.username = "bgottlob";
   home.homeDirectory = "/home/bgottlob";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+  # This should be the same value as `system.stateVersion` in `configuration.nix`
+  home.stateVersion = "23.11";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
