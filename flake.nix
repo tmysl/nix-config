6a3b4b352c19f0baf353ee97c6e2d0e62026977c
Brandon Gottlob <brandon.gottlob@onepeloton.com>
2024-01-04 22:31:05 -0500

    Add nix-darwin as a flake input

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/acro/home.nix
+++ hosts/acro/home.nix
@@ -5,7 +5,7 @@
   home.homeDirectory = "/Users/brandon.gottlob";
 
   # This should be the same value as `system.stateVersion` in `configuration.nix`
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
