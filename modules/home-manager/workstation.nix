10c89de121d09161d48467b05343a9a6b0585de1
Brandon Gottlob <brandon.gottlob@onepeloton.com>
2024-01-09 13:40:16 -0500

    Add jq, yq, and nix-direnv

--- modules/home-manager/workstation.nix
+++ modules/home-manager/workstation.nix
@@ -6,18 +6,24 @@
     bat
     keepassxc
     libreoffice-qt
+    jq
+    yq
   ];
 
   home.file = {
     ".inputrc".source = ../../dotfiles/inputrc;
   };
 
-  programs.direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
 
   imports = [
+    ./k8s.nix
     ./kitty.nix
-    ./vim.nix
     ./tmux.nix
+    ./vim.nix
     ./zsh.nix
   ];
 }
