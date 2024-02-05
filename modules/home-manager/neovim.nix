00f3f557a10399f3090904b1de590146372183a5
Brandon Gottlob <info@bgottlob.com>
2024-02-04 19:46:49 -0500

    Install vim with lighter plugin set than neovim

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -1,34 +1,44 @@
 { config, pkgs, ... }:
 
-{
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    extraConfig = builtins.readFile ../../dotfiles/vimrc;
-    plugins = with pkgs.vimPlugins; [
-      auto-pairs
-      csv-vim
-      markdown-preview-nvim
-      vim-abolish
-      vim-colors-solarized
-      vim-elixir
-      vim-erlang-runtime
-      vim-javascript
-      vim-json
-      vim-nix
-      vim-ruby
-      vim-sleuth
-      vim-slime
-      vim-speeddating
-      vim-surround
-      vim-terraform
-      vim-tmux-navigator
-      vim-unimpaired
-      vim-yaml
-      vimtex
-    ];
-  };
-}
+let
+  vimrcSrc = builtins.readFile ../../dotfiles/vimrc;
+
+  vimPlugins = with pkgs.vimPlugins; [
+    auto-pairs
+    csv-vim
+    vim-abolish
+    vim-colors-solarized
+    vim-elixir
+    vim-erlang-runtime
+    vim-javascript
+    vim-json
+    vim-nix
+    vim-ruby
+    vim-sleuth
+    vim-slime
+    vim-speeddating
+    vim-surround
+    vim-terraform
+    vim-tmux-navigator
+    vim-unimpaired
+    vim-yaml
+    vimtex
+  ];
+in
+  {
+    # Keep vim lighter than neovim
+    programs.vim = {
+      enable = true;
+      defaultEditor = true;
+      extraConfig = vimrcSrc;
+      plugins = vimPlugins;
+    };
+
+    programs.neovim = {
+      enable = true;
+      extraConfig = vimrcSrc;
+      plugins = with pkgs.vimPlugins; [
+        markdown-preview-nvim
+      ] ++ vimPlugins;
+    };
+  }
